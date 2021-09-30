efc837e6f41ac788953b21180197545694af3619
david <davidmatovu88@gmail.com>
2021-09-30 15:02:27 +0300

    Upadated the display

--- Calculator/styles/app.js
+++ Calculator/styles/app.js
@@ -4,14 +4,13 @@ const operationButtons = document.querySelectorAll('.operator');
 const equalsButton = document.querySelector('.equals');
 const deleteButton = document.querySelector('.delete');
 const resetButton = document.querySelector('.reset');
-const display = document.querySelector('output');
+//const display = document.querySelector('output');
 const displayElement1 = document.querySelector('.previous-number');
 const displayElement2 = document.querySelector('.current-number');
-let displayValue;
 let prev = '';
 let current = '';
 let result = null;
-let lastOperation = '';
+
 let decimalPoint = false;
 
 
@@ -25,42 +24,70 @@ let decimalPoint = false;
      } else if(e.target.innerText === "." && decimalPoint){
         return;
      }
+     else{
+         prev += e.target.innerText;
+     }
      current += e.target.innerText;
      displayElement2.innerText = current;
+     displayElement1.innerText = prev;
+     
      console.log(current)
+     //console.log(prev)
+     //calculate();
     }
 
 
     operationButtons.forEach(operator => {
         operator.addEventListener('click',(e)=>{
+            if(operator!=='='){// If the operator is not equals
             operator = e.target.innerText;
-            //display= e.target.innerText;
+            operationButtons.textcontent= operator
             console.log(operator)
+           
+        }else{
             console.log(prev)
-
+        }
+      
         });
     });
     
+    function calculate(){
+        let result;
+       // displayElement2.innerText = result;
+        let operation;
+        switch (operation) {
+          case '+':
+            result= parseFloat(prev) + parseFloat(current)
+            console.log(result);
+            break;
+          case '-':
+            result= prev - current
+            break
+          case 'X':
+            result = prev * current
+            break
+          case '/':
+            result = prev / current
+            break
+          default:
+            return
+        }
+    }
 
-    // calculate() {
-    //     let computation;
-    //     const prev = parseFloat(previousNumber)
-    //     const current = parseFloat(currentNumber)
-    //     if (isNaN(prev) || isNaN(current)) return
-    //     switch (operator) {
-    //       case '+':
-    //         computation = prev + current
-    //         break
-    //       case '-':
-    //         computation = prev - current
-    //         break
-    //       case '*':
-    //         computation = prev * current
-    //         break
-    //       case '÷':
-    //         computation = prev / current
-    //         break
-    //       default:
-    //         return
-    //     }
-    // }
+
+function clearLast(){
+
+}
+deleteButton.addEventListener('click', button => {
+    
+  })
+  function clearAll(){
+
+  }
+  resetButton.addEventListener('click', button =>{
+      
+  })
+
+  equalsButton.addEventListener('click', button => {
+ calculate();
+  })
